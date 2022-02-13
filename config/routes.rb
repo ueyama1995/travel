621f972fc66493933ecdc813a051439dbdcd1a5a
ueyama1995 <shinji160829033205@gmail.com>
2022-02-13 03:55:16 +0000

    [update]users　controller追加他

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'travels/index'
-  get 'travels/show'
-  get 'travels/edit'
-  get 'travels/new'
+  resources :users, only: [:index, :show, :edit]
+  resources :travels, only: [:index, :new, :show, :edit]
   devise_for :users
   root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
