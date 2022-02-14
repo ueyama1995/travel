6432f4450fe580993df99d7c9d5a7a86e85a7b2c
ueyama1995 <shinji160829033205@gmail.com>
2022-02-14 12:01:49 +0000

    [update]viewを少しいじった

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :travels, only: [:index, :new, :show, :edit]
   devise_for :users
   root to: 'homes#top'
+  get 'users/check' => 'users#check'
+  get 'users' => redirect("users/sign_in")
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
